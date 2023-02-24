17cff45ca95e5608c9ba847d78ef36942c827364
leonardorodrigues42 <leonardorodriguesaai@outlook.com>
2023-02-24 13:54:02 -0300

    dev: test case

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata"
 import "express-async-errors"
 import express from "express"
+import cors from "cors"
 import handleErrorMiddleware from "./middlewares/HandleError.middleware"
 import appRouter from "./routers"
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 appRouter(app)
 app.use(handleErrorMiddleware)
