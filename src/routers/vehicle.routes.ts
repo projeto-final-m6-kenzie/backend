b9f5b25dc5bd0135e29c4c72822f79cefce41b92
Rayan Brasil <brasrayan@gmail.com>
2023-02-27 17:34:57 -0400

    feat: vehicleImgs delete

--- src/routers/vehicle.routes.ts
+++ src/routers/vehicle.routes.ts
@@ -1,20 +1,26 @@
-import { Router } from "express";
-import createVehicleController from "../controllers/vehicles/createVehicle.controller";
-import getVehicleByIdController from "../controllers/vehicles/getUserById.controller";
-import getVehiclesController from "../controllers/vehicles/getVehicles.controller";
-import updateVehicleController from "../controllers/vehicles/updateVehicle.controller";
-import userAuthMiddleware from "../middlewares/userAuth.middleware";
-import deleteVehicleService from "../services/vehicles/deleteVehicle.service";
+import { Router } from 'express';
+import createVehicleController from '../controllers/vehicles/createVehicle.controller';
+import deleteVehicleImageController from '../controllers/vehicles/deleteVehiclePhoto.controller';
+import getVehicleByIdController from '../controllers/vehicles/getUserById.controller';
+import getVehiclesController from '../controllers/vehicles/getVehicles.controller';
+import updateVehicleController from '../controllers/vehicles/updateVehicle.controller';
+import userAuthMiddleware from '../middlewares/userAuth.middleware';
+import deleteVehicleService from '../services/vehicles/deleteVehicle.service';
 
 const vehicleRouter = () => {
-    const router = Router();
-    router.post('', userAuthMiddleware ,createVehicleController);
-    router.get('', getVehiclesController);
-    router.get('/:id', getVehicleByIdController);
-    router.patch('/:id', userAuthMiddleware ,updateVehicleController)
-    router.delete('/:id', userAuthMiddleware ,deleteVehicleService)
-  
-    return router;
-  };
-  
-  export default vehicleRouter;
+  const router = Router();
+  router.post('', userAuthMiddleware, createVehicleController);
+  router.get('', getVehiclesController);
+  router.get('/:id', getVehicleByIdController);
+  router.patch('/:id', userAuthMiddleware, updateVehicleController);
+  router.delete('/:id', userAuthMiddleware, deleteVehicleService);
+  router.delete(
+    '/photos/:id',
+    userAuthMiddleware,
+    deleteVehicleImageController
+  );
+
+  return router;
+};
+
+export default vehicleRouter;
