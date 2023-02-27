133fc7bcb3d9d73366b8ab5b507dc2a6a91dc58c
QuerenHope <querenhope@hotmail.com>
2023-02-27 14:07:36 -0300

    feat: criaçao do comments

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,13 @@
 import { Express } from 'express';
+import commentRouter from './comment.routes';
 import userRouter from './user.routes';
 import vehicleRouter from './vehicle.routes';
 
 const appRouter = (app: Express) => {
   app.use('/users', userRouter());
   app.use('/vehicles', vehicleRouter())
+  app.use('/comment', commentRouter())
+
 };
 
 export default appRouter;
